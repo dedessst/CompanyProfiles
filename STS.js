c8624d17024bc680483c37db3260f51b4e5970a7
dedessst <144593133+dedessst@users.noreply.github.com>
2023-10-04 07:57:45 +0700

    Add files via upload

---
+++ STS.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
+
+
+
+/**
+ * search toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    searchBox.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * scroll reveal
+ */
+	ScrollReveal({
+		reset: true,
+		distance: '80px',
+		duration: 2000,
+		delay: 200
+	});
+
+ScrollReveal().reveal('.hero-content', { origin: 'top'});
+ScrollReveal().reveal('.footer-link, .footer-link-title', { origin: 'bottom'});
+ScrollReveal().reveal('.brand-item', { origin: 'left'});
+ScrollReveal().reveal('.featured-game-card, .shop-card', { origin: 'right'});
